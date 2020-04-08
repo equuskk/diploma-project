338005dd4b42ba576144a924220319e6daf45694
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-04-08 15:05:09 +0300

    Init

---
+++ DiplomaProject.WebApp/Program.cs
@@ -0,0 +1,37 @@
+using System.Globalization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace DiplomaProject.WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SetDefaultLocale();
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static void SetDefaultLocale()
+        {
+            var culture = new CultureInfo("ru-RU");
+            CultureInfo.CurrentCulture = culture;
+            CultureInfo.CurrentUICulture = culture;
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                       .ConfigureLogging(config => { config.ClearProviders(); })
+                       .UseSerilog((hostingContext, loggerConfiguration) =>
+                       {
+                           loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration);
+                       })
+                       .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        }
+    }
+}

7b4ec5211f68a4252f13e3f699fdede02f42592b
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-05-21 23:56:28 +0300

    Добавить тест на проверку связи между сектором и литоралью

--- tests/DiplomaProject.Application.UnitTests/Sectors/Commands/CreateSectorCommandTests.cs
+++ tests/DiplomaProject.Application.UnitTests/Sectors/Commands/CreateSectorCommandTests.cs
@@ -1,7 +1,9 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using DiplomaProject.Application.Sectors.Commands;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace DiplomaProject.Application.UnitTests.Sectors.Commands
@@ -23,6 +25,20 @@ namespace DiplomaProject.Application.UnitTests.Sectors.Commands
             result.Id.Should().Be(4);
             result.Square.Should().Be(123);
             result.LitoralId.Should().Be(1);
+        }
+
+        [Fact]
+        public async Task ShouldThrowException()
+        {
+            var command = new CreateSectorCommand
+            {
+                Square = 123,
+                LitoralId = 1123
+            };
+            var handler = new CreateSectorCommandHandler(ApplicationContext);
+
+            Func<Task> func = async () => await handler.Handle(command, CancellationToken.None);
+            await func.Should().ThrowAsync<DbUpdateException>("Некорректный id литорали");
         }
     }
 }

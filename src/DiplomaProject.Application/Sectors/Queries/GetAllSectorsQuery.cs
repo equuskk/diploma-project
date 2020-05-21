01faa7d1744383efc3877b207b1ca5efa5f91ffb
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-05-21 19:50:38 +0300

    Добавить запрос на получение секторов

---
+++ src/DiplomaProject.Application/Sectors/Queries/GetAllSectorsQuery.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.DataAccess;
+using DiplomaProject.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiplomaProject.Application.Sectors.Queries
+{
+    public class GetAllSectorsQueryHandler : IRequestHandler<GetAllSectorsQuery, Sector[]>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GetAllSectorsQueryHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Sector[]> Handle(GetAllSectorsQuery request, CancellationToken cancellationToken)
+        {
+            return _context.Sectors.ToArrayAsync(cancellationToken);
+        }
+    }
+
+    public class GetAllSectorsQuery : IRequest<Sector[]>
+    {
+    }
+}
---
+++ tests/DiplomaProject.Application.UnitTests/Sectors/Queries/GetAllSectorsQueryTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.Application.Sectors.Queries;
+using FluentAssertions;
+using Xunit;
+
+namespace DiplomaProject.Application.UnitTests.Sectors.Queries
+{
+    public class GetAllSectorsQueryTests : TestBase
+    {
+        [Fact]
+        public async Task ShouldReturnSectors()
+        {
+            var command = new GetAllSectorsQuery();
+            var handler = new GetAllSectorsQueryHandler(ApplicationContext);
+
+            var result = await handler.Handle(command, CancellationToken.None);
+            var first = result.First();
+
+            result.Count().Should().Be(3);
+            first.Id.Should().Be(1);
+            first.LitoralId.Should().Be(1);
+            first.Square.Should().Be(111);
+        }
+    }
+}

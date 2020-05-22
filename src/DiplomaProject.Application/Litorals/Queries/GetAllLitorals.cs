f95bc59e4c1082ef0781a5aff1aa35786a4ba8a7
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-05-22 17:19:21 +0300

    Добавить запрос и страницу для всех литоралей

---
+++ src/DiplomaProject.Application/Litorals/Queries/GetAllLitorals.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.DataAccess;
+using DiplomaProject.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+namespace DiplomaProject.Application.Litorals.Queries
+{
+    public class GetAllLitoralsHandler : IRequestHandler<GetAllLitorals, Litoral[]>
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger _logger;
+
+        public GetAllLitoralsHandler(ApplicationDbContext context)
+        {
+            _context = context;
+            _logger = Log.ForContext<GetAllLitorals>();
+        }
+
+        public Task<Litoral[]> Handle(GetAllLitorals request, CancellationToken cancellationToken)
+        {
+            _logger.Debug("Получение списка всех литоралей");
+            return _context.Litorals.ToArrayAsync(cancellationToken);
+        }
+    }
+
+    public class GetAllLitorals : IRequest<Litoral[]>
+    {
+    }
+}
---
+++ tests/DiplomaProject.Application.UnitTests/Litorals/Queries/GetAllLitoralsTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.Application.Litorals.Queries;
+using FluentAssertions;
+using Xunit;
+
+namespace DiplomaProject.Application.UnitTests.Litorals.Queries
+{
+    public class GetAllLitoralsTests : TestBase
+    {
+        [Fact]
+        public async Task ShouldReturnLitorals()
+        {
+            var command = new GetAllLitorals();
+            var handler = new GetAllLitoralsHandler(ApplicationContext);
+
+            var result = await handler.Handle(command, CancellationToken.None);
+            var first = result.First();
+
+            result.Should().HaveCount(4);
+            first.Title.Should().Be("Скальная литораль");
+        }
+    }
+}

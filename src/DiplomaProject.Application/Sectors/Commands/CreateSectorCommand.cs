3082bf4285845f72cb69fa0f86f6ff357acab403
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-05-22 15:56:05 +0300

    Добавить логи в уже существующие команды и запросы

--- src/DiplomaProject.Application/Sectors/Commands/CreateSectorCommand.cs
+++ src/DiplomaProject.Application/Sectors/Commands/CreateSectorCommand.cs
@@ -3,23 +3,28 @@ using System.Threading.Tasks;
 using DiplomaProject.DataAccess;
 using DiplomaProject.Domain.Entities;
 using MediatR;
+using Serilog;
 
 namespace DiplomaProject.Application.Sectors.Commands
 {
     public class CreateSectorCommandHandler : IRequestHandler<CreateSectorCommand, Sector>
     {
         private readonly ApplicationDbContext _context;
+        private readonly ILogger _logger;
 
         public CreateSectorCommandHandler(ApplicationDbContext context)
         {
             _context = context;
+            _logger = Log.ForContext<CreateSectorCommandHandler>();
         }
 
         public async Task<Sector> Handle(CreateSectorCommand request, CancellationToken cancellationToken)
         {
+            _logger.Debug("Создание нового сектора");
             var sector = new Sector(request.Square, request.LitoralId);
             await _context.Sectors.AddAsync(sector, cancellationToken);
             await _context.SaveChangesAsync(cancellationToken);
+            _logger.Debug("Создан сектор с ID {SectorId}", sector.Id);
 
             return sector;
         }
--- src/DiplomaProject.Application/Sectors/Commands/DeleteSectorCommand.cs
+++ src/DiplomaProject.Application/Sectors/Commands/DeleteSectorCommand.cs
@@ -5,28 +5,34 @@ using DiplomaProject.Domain.Entities;
 using DiplomaProject.Domain.Exceptions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 namespace DiplomaProject.Application.Sectors.Commands
 {
     public class DeleteSectorCommandHandler : IRequestHandler<DeleteSectorCommand, Unit>
     {
         private readonly ApplicationDbContext _context;
+        private readonly ILogger _logger;
 
         public DeleteSectorCommandHandler(ApplicationDbContext context)
         {
             _context = context;
+            _logger = Log.ForContext<DeleteSectorCommandHandler>();
         }
 
         public async Task<Unit> Handle(DeleteSectorCommand request, CancellationToken cancellationToken)
         {
+            _logger.Debug("Удаление сектора с Id {SectorId}", request.SectorId);
             var sector = await _context.Sectors.SingleOrDefaultAsync(x => x.Id == request.SectorId, cancellationToken);
             if(sector is null)
             {
+                _logger.Warning("Сектор с Id {SectorId} не найден", request.SectorId);
                 throw new NotFoundException(request.SectorId, nameof(Sector));
             }
 
             _context.Sectors.Remove(sector);
             await _context.SaveChangesAsync(cancellationToken);
+            _logger.Information("Удалён сектор с Id {SectorId}", request.SectorId);
 
             return Unit.Value;
         }

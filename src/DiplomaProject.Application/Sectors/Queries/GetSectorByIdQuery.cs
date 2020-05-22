0f521a8cf1e735308062579d2371cc5ac7d09b79
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-05-22 15:40:50 +0300

    Добавить запрос получение сектора по ID

---
+++ src/DiplomaProject.Application/Sectors/Queries/GetSectorByIdQuery.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.DataAccess;
+using DiplomaProject.Domain.Entities;
+using DiplomaProject.Domain.Exceptions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiplomaProject.Application.Sectors.Queries
+{
+    public class GetSectorByIdQueryHandler : IRequestHandler<GetSectorByIdQuery, Sector>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GetSectorByIdQueryHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Sector> Handle(GetSectorByIdQuery request, CancellationToken cancellationToken)
+        {
+            var sector = await _context.Sectors.Include(x => x.Bioresources)
+                                       .Include(x => x.Expeditions)
+                                       .SingleOrDefaultAsync(x => x.Id == request.SectorId, cancellationToken);
+            if(sector is null)
+            {
+                throw new NotFoundException(request.SectorId, nameof(Sector));
+            }
+
+            return sector;
+        }
+    }
+
+    public class GetSectorByIdQuery : IRequest<Sector>
+    {
+        public int SectorId { get; set; }
+    }
+}
---
+++ tests/DiplomaProject.Application.UnitTests/Sectors/Queries/GetSectorByIdQueryTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.Application.Sectors.Queries;
+using DiplomaProject.Domain.Exceptions;
+using FluentAssertions;
+using Xunit;
+
+namespace DiplomaProject.Application.UnitTests.Sectors.Queries
+{
+    public class GetSectorByIdQueryTests : TestBase
+    {
+        [Fact]
+        public async Task ShouldReturnSectorWithThatId()
+        {
+            var command = new GetSectorByIdQuery
+            {
+                SectorId = 1
+            };
+            var handler = new GetSectorByIdQueryHandler(ApplicationContext);
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            result.Id.Should().Be(1);
+            result.LitoralId.Should().Be(1);
+            result.Square.Should().Be(111);
+            result.Litoral.Should().NotBeNull();
+            result.Bioresources.Should().NotBeNull();
+            result.Expeditions.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task ShouldThrowNotFoundException()
+        {
+            var command = new GetSectorByIdQuery
+            {
+                SectorId = 11111
+            };
+            var handler = new GetSectorByIdQueryHandler(ApplicationContext);
+
+            Func<Task> func = async () => await handler.Handle(command, CancellationToken.None);
+            await func.Should().ThrowAsync<NotFoundException>();
+        }
+    }
+}

26f72a33d4f4d8583399057f805c478547beb7ac
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-02 16:11:07 +0300

    Добавить команду создания экспедиции

---
+++ src/DiplomaProject.Application/Expeditions/Commands/CreateExpeditionCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.DataAccess;
+using DiplomaProject.Domain.Entities;
+using MediatR;
+
+namespace DiplomaProject.Application.Expeditions.Commands
+{
+    public class CreateExpeditionCommandHandler : IRequestHandler<CreateExpeditionCommand, Expedition>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateExpeditionCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Expedition> Handle(CreateExpeditionCommand request, CancellationToken cancellationToken)
+        {
+            if(request.FromDate > request.ToDate)
+            {
+                throw new ArgumentException("Дата окончания экспедиции меньше даты начала");
+            }
+
+            var expedition = new Expedition
+            {
+                FromDate = request.FromDate,
+                ToDate = request.ToDate
+            };
+
+            await _context.Expeditions.AddAsync(expedition, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return expedition;
+        }
+    }
+
+    public class CreateExpeditionCommand : IRequest<Expedition>
+    {
+        public DateTimeOffset FromDate { get; set; }
+        public DateTimeOffset ToDate { get; set; }
+    }
+}
---
+++ tests/DiplomaProject.Application.UnitTests/Expedition/Commands/CreateExpeditionCommandTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.Application.Expeditions.Commands;
+using FluentAssertions;
+using Xunit;
+
+namespace DiplomaProject.Application.UnitTests.Expedition.Commands
+{
+    public class CreateExpeditionCommandTests : TestBase
+    {
+        [Fact]
+        public async Task ShouldCreateExpedition()
+        {
+            var fromDate = new DateTimeOffset(2019, 01, 01, 0, 0, 0, TimeSpan.Zero);
+            var toDate = fromDate.AddMonths(1);
+            var command = new CreateExpeditionCommand
+            {
+                FromDate = fromDate,
+                ToDate = toDate
+            };
+            var handler = new CreateExpeditionCommandHandler(ApplicationContext);
+
+            var result = await handler.Handle(command, CancellationToken.None);
+            result.Id.Should().Be(1);
+            result.FromDate.Should().Be(fromDate);
+            result.ToDate.Should().Be(toDate);
+        }
+
+        [Fact]
+        public async Task ShouldThrowException()
+        {
+            var fromDate = new DateTimeOffset(2019, 01, 01, 0, 0, 0, TimeSpan.Zero);
+            var toDate = fromDate.AddMonths(-1);
+            var command = new CreateExpeditionCommand
+            {
+                FromDate = fromDate,
+                ToDate = toDate
+            };
+            var handler = new CreateExpeditionCommandHandler(ApplicationContext);
+
+            Func<Task> func = async () => await handler.Handle(command, CancellationToken.None);
+            await func.Should().ThrowAsync<ArgumentException>();
+        }
+    }
+}

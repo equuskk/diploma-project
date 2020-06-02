6d94d43b328e1e12f6c8a2afea069725d7daa533
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-02 17:35:07 +0300

    Добавить команду получения экспедиций

---
+++ src/DiplomaProject.Application/Expeditions/Queries/GetAllExpeditionsQuery.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.DataAccess;
+using DiplomaProject.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiplomaProject.Application.Expeditions.Queries
+{
+    public class GetAllExpeditionsQueryHandler : IRequestHandler<GetAllExpeditionsQuery, Expedition[]>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GetAllExpeditionsQueryHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Expedition[]> Handle(GetAllExpeditionsQuery request, CancellationToken cancellationToken)
+        {
+            return _context.Expeditions.ToArrayAsync(cancellationToken);
+        }
+    }
+
+    public class GetAllExpeditionsQuery : IRequest<Expedition[]>
+    {
+    }
+}
---
+++ tests/DiplomaProject.Application.UnitTests/Expedition/Queries/GetAllExpeditionsQueryTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.Application.Expeditions.Queries;
+using FluentAssertions;
+using Xunit;
+
+namespace DiplomaProject.Application.UnitTests.Expedition.Queries
+{
+    public class GetAllExpeditionsQueryTests : TestBase
+    {
+        [Fact]
+        public async Task ShouldReturnExpeditions()
+        {
+            var command = new GetAllExpeditionsQuery();
+            var handler = new GetAllExpeditionsQueryHandler(ApplicationContext);
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            result.Should().HaveCount(1);
+            result.First().Should().NotBeNull();
+        }
+    }
+}

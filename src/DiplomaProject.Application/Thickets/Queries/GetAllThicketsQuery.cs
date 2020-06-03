1ee64de8ae5eb581cb55a064d13fee0d7ede1aa2
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-03 17:41:42 +0300

    Добавить команду с получением всех зарослей

---
+++ src/DiplomaProject.Application/Thickets/Queries/GetAllThicketsQuery.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.DataAccess;
+using DiplomaProject.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiplomaProject.Application.Thickets.Queries
+{
+    public class GetAllThicketsQueryHandler : IRequestHandler<GetAllThicketsQuery, Thicket[]>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GetAllThicketsQueryHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Thicket[]> Handle(GetAllThicketsQuery request, CancellationToken cancellationToken)
+        {
+            return _context.Thickets
+                           .Include(x => x.Litoral)
+                           .Include(x => x.Seaweed)
+                           .Include(x => x.Sector)
+                           .Include(x => x.GroundType)
+                           .ToArrayAsync(cancellationToken);
+        }
+    }
+
+    public class GetAllThicketsQuery : IRequest<Thicket[]>
+    {
+    }
+}
---
+++ tests/DiplomaProject.Application.UnitTests/Thickets/Queries/GetAllThicketsQueryTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.Application.Thickets.Queries;
+using FluentAssertions;
+using Xunit;
+
+namespace DiplomaProject.Application.UnitTests.Thickets.Queries
+{
+    public class GetAllThicketsQueryTests : TestBase
+    {
+        [Fact]
+        public async Task ShouldReturnThickets()
+        {
+            var command = new GetAllThicketsQuery();
+            var handler = new GetAllThicketsQueryHandler(ApplicationContext);
+
+            var result = await handler.Handle(command, CancellationToken.None);
+            var first = result.First();
+
+            result.Should().HaveCount(1);
+            first.Litoral.Should().NotBeNull();
+            first.Sector.Should().NotBeNull();
+            first.GroundType.Should().NotBeNull();
+            first.Seaweed.Should().NotBeNull();
+        }
+    }
+}

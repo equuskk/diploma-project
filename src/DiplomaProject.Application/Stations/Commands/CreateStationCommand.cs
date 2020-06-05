20009ea1dda58ecf3637dde44f131b446ec20e89
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-05 20:47:48 +0300

    Добавить команду с созданием станции

---
+++ src/DiplomaProject.Application/Stations/Commands/CreateStationCommand.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.DataAccess;
+using DiplomaProject.Domain.Entities;
+using MediatR;
+using NetTopologySuite.Geometries;
+
+namespace DiplomaProject.Application.Stations.Commands
+{
+    public class CreateStationCommandHandler : IRequestHandler<CreateStationCommand, Station>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateStationCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Station> Handle(CreateStationCommand request, CancellationToken cancellationToken)
+        {
+            var item = new Station
+            {
+                Location = request.Location,
+                ThicketId = request.ThicketId
+            };
+            await _context.Stations.AddAsync(item, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return item;
+        }
+    }
+
+    public class CreateStationCommand : IRequest<Station>
+    {
+        public Point Location { get; set; }
+        public int ThicketId { get; set; }
+    }
+}
---
+++ tests/DiplomaProject.Application.UnitTests/Stations/Commands/CreateStationCommandTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.Application.Stations.Commands;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NetTopologySuite.Geometries;
+using Xunit;
+
+namespace DiplomaProject.Application.UnitTests.Stations.Commands
+{
+    public class CreateStationCommandTests : TestBase
+    {
+        [Fact]
+        public async Task ShouldCreateStation()
+        {
+            var command = new CreateStationCommand
+            {
+                Location = new Point(1.1, 1.1),
+                ThicketId = 1
+            };
+            var handler = new CreateStationCommandHandler(ApplicationContext);
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            result.Id.Should().Be(2);
+        }
+
+        [Fact]
+        public async Task ShouldThrowException_BecauseThicketIdIsIncorrect()
+        {
+            var command = new CreateStationCommand
+            {
+                Location = new Point(1.1, 1.1),
+                ThicketId = -1
+            };
+            var handler = new CreateStationCommandHandler(ApplicationContext);
+
+            Func<Task> func = async () => await handler.Handle(command, CancellationToken.None);
+
+            await func.Should().ThrowAsync<DbUpdateException>();
+        }
+    }
+}

c866ede865a837688a3c1bedfe3917a54bf02246
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-05 21:08:22 +0300

    Добавить команду с удалением станции

---
+++ src/DiplomaProject.Application/Stations/Commands/DeleteStationCommand.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.DataAccess;
+using DiplomaProject.Domain.Entities;
+using DiplomaProject.Domain.Exceptions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiplomaProject.Application.Stations.Commands
+{
+    public class DeleteStationCommandHandler : IRequestHandler<DeleteStationCommand, Unit>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DeleteStationCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteStationCommand request, CancellationToken cancellationToken)
+        {
+            var item = await _context.Stations.SingleOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+            if(item is null)
+            {
+                throw new NotFoundException(request.Id, nameof(Station));
+            }
+
+            _context.Stations.Remove(item);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+
+    public class DeleteStationCommand : IRequest<Unit>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ tests/DiplomaProject.Application.UnitTests/Stations/Commands/DeleteStationCommandTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.Application.Stations.Commands;
+using DiplomaProject.Domain.Exceptions;
+using FluentAssertions;
+using Xunit;
+
+namespace DiplomaProject.Application.UnitTests.Stations.Commands
+{
+    public class DeleteStationCommandTests : TestBase
+    {
+        [Fact]
+        public async Task ShouldDeleteStation()
+        {
+            var command = new DeleteStationCommand
+            {
+                Id = 1
+            };
+            var handler = new DeleteStationCommandHandler(ApplicationContext);
+
+            _ = await handler.Handle(command, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task ShouldThrowException_BecauseIdIsIncorrect()
+        {
+            var command = new DeleteStationCommand
+            {
+                Id = -1
+            };
+            var handler = new DeleteStationCommandHandler(ApplicationContext);
+
+            Func<Task> func = async () => await handler.Handle(command, CancellationToken.None);
+            await func.Should().ThrowAsync<NotFoundException>();
+        }
+    }
+}

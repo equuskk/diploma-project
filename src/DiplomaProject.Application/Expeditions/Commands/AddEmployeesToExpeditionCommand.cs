e9cd5bb659f8df232da013d1712444bd7fd1de33
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-05 18:16:01 +0300

    Добавить команду для добавления сотрудников к экспедиции

---
+++ src/DiplomaProject.Application/Expeditions/Commands/AddEmployeesToExpeditionCommand.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.DataAccess;
+using DiplomaProject.Domain.Entities;
+using MediatR;
+
+namespace DiplomaProject.Application.Expeditions.Commands
+{
+    public class AddEmployeesToExpeditionCommandHandler : IRequestHandler<AddEmployeesToExpeditionCommand, Unit>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AddEmployeesToExpeditionCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(AddEmployeesToExpeditionCommand request, CancellationToken cancellationToken)
+        {
+            var items = request.UserIds.Select(userId => new EmployeeExpedition
+            {
+                ExpeditionId = request.ExpeditionId,
+                EmployeeId = userId
+            });
+
+            await _context.EmployeeExpeditions.AddRangeAsync(items, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+
+    public class AddEmployeesToExpeditionCommand : IRequest<Unit>
+    {
+        public int ExpeditionId { get; set; }
+        public string[] UserIds { get; set; }
+    }
+}
---
+++ tests/DiplomaProject.Application.UnitTests/Expedition/Commands/AddEmployeesToExpeditionCommandTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.Application.Expeditions.Commands;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace DiplomaProject.Application.UnitTests.Expedition.Commands
+{
+    public class AddEmployeesToExpeditionCommandTest : TestBase
+    {
+        [Fact]
+        public async Task ShouldAddSectorsToExpedition()
+        {
+            var command = new AddEmployeesToExpeditionCommand
+            {
+                ExpeditionId = 1,
+                UserIds = new[] { UserId }
+            };
+            var handler = new AddEmployeesToExpeditionCommandHandler(ApplicationContext);
+
+            _ = await handler.Handle(command, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task ShouldThrowException_BecauseExpeditionIdIsIncorrect()
+        {
+            var command = new AddEmployeesToExpeditionCommand
+            {
+                ExpeditionId = -1,
+                UserIds = new[] { UserId }
+            };
+            var handler = new AddEmployeesToExpeditionCommandHandler(ApplicationContext);
+
+            Func<Task> func = async () => await handler.Handle(command, CancellationToken.None);
+            await func.Should().ThrowAsync<DbUpdateException>();
+        }
+
+        [Fact]
+        public async Task ShouldThrowException_BecauseSectorIdsAreIncorrect()
+        {
+            var command = new AddEmployeesToExpeditionCommand
+            {
+                ExpeditionId = 1,
+                UserIds = new[] { Guid.NewGuid().ToString() }
+            };
+            var handler = new AddEmployeesToExpeditionCommandHandler(ApplicationContext);
+
+            Func<Task> func = async () => await handler.Handle(command, CancellationToken.None);
+            await func.Should().ThrowAsync<DbUpdateException>();
+        }
+    }
+}

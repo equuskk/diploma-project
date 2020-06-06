0298e42e2a1cfcf94f2f09d87139eb84c511a540
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-06 21:42:09 +0300

    Добавить команду добавления данных со станции

---
+++ src/DiplomaProject.Application/StationsData/Commands/CreateStationDataCommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.DataAccess;
+using DiplomaProject.Domain.Entities;
+using DiplomaProject.Domain.Exceptions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiplomaProject.Application.StationsData.Commands
+{
+    public class CreateStationDataCommandHandler : IRequestHandler<CreateStationDataCommand, StationData>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateStationDataCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<StationData> Handle(CreateStationDataCommand request, CancellationToken cancellationToken)
+        {
+            var isStationExists = await _context.Stations.AnyAsync(x => x.Id == request.StationId, cancellationToken);
+            if(!isStationExists)
+            {
+                throw new NotFoundException(request.StationId, nameof(Station));
+            }
+
+            var stationData = new StationData
+            {
+                Date = request.Date,
+                Temperature = request.Temperature,
+                Density = request.Density,
+                Depth = request.Depth,
+                StationId = request.StationId
+            };
+            await _context.StationsData.AddAsync(stationData, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return stationData;
+        }
+    }
+
+    public class CreateStationDataCommand : IRequest<StationData>
+    {
+        public DateTimeOffset Date { get; set; }
+        public float Temperature { get; set; }
+        public float Density { get; set; }
+        public float Depth { get; set; }
+
+        public int StationId { get; set; }
+    }
+}
---
+++ tests/DiplomaProject.Application.UnitTests/StationsData/Commands/CreateStationDataCommandTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.Application.StationsData.Commands;
+using DiplomaProject.Domain.Exceptions;
+using FluentAssertions;
+using Xunit;
+
+namespace DiplomaProject.Application.UnitTests.StationsData.Commands
+{
+    public class CreateStationDataCommandTests : TestBase
+    {
+        [Fact]
+        public async Task ShouldCreateStationData()
+        {
+            var command = new CreateStationDataCommand
+            {
+                Date = new DateTimeOffset(2019, 01, 01, 01, 01, 01, TimeSpan.Zero),
+                Density = 10,
+                Depth = 0.2f,
+                StationId = 1,
+                Temperature = -7.3f
+            };
+            var handler = new CreateStationDataCommandHandler(ApplicationContext);
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            result.Id.Should().Be(2);
+        }
+
+        [Fact]
+        public async Task ShouldThrowException_BecauseStationIdIsIncorrect()
+        {
+            var command = new CreateStationDataCommand
+            {
+                Date = new DateTimeOffset(2019, 01, 01, 01, 01, 01, TimeSpan.Zero),
+                Density = 10,
+                Depth = 0.2f,
+                StationId = -1,
+                Temperature = -7.3f
+            };
+            var handler = new CreateStationDataCommandHandler(ApplicationContext);
+
+            Func<Task> func = async () => await handler.Handle(command, CancellationToken.None);
+
+            await func.Should().ThrowAsync<NotFoundException>();
+        }
+    }
+}

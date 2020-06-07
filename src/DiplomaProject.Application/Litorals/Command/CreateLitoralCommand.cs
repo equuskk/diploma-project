c889048f891f73ec50195ce280bb7d25db7b64ef
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-07 20:50:57 +0300

    Добавить команду создания литорали

---
+++ src/DiplomaProject.Application/Litorals/Command/CreateLitoralCommand.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.DataAccess;
+using DiplomaProject.Domain.Entities;
+using MediatR;
+
+namespace DiplomaProject.Application.Litorals.Command
+{
+    public class CreateLitoralCommandHandler : IRequestHandler<CreateLitoralCommand, Litoral>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateLitoralCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Litoral> Handle(CreateLitoralCommand request, CancellationToken cancellationToken)
+        {
+            var item = new Litoral(request.Title);
+
+            await _context.AddAsync(item, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return item;
+        }
+    }
+
+    public class CreateLitoralCommand : IRequest<Litoral>
+    {
+        public string Title { get; set; }
+    }
+}
---
+++ tests/DiplomaProject.Application.UnitTests/Litorals/Commands/CreateLitoralCommandTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.Application.Litorals.Command;
+using FluentAssertions;
+using Xunit;
+
+namespace DiplomaProject.Application.UnitTests.Litorals.Commands
+{
+    public class CreateLitoralCommandTests : TestBase
+    {
+        [Fact]
+        public async Task ShouldCreateLitoral()
+        {
+            var command = new CreateLitoralCommand
+            {
+                Title = "Title"
+            };
+            var handler = new CreateLitoralCommandHandler(ApplicationContext);
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            result.Id.Should().Be(5);
+        }
+    }
+}

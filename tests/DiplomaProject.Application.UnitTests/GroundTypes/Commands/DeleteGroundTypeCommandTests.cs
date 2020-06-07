31d2060c8beb605e7d12af9fda10b12986e33c36
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-07 20:41:00 +0300

    Добавить команду удаления типа грунта

---
+++ tests/DiplomaProject.Application.UnitTests/GroundTypes/Commands/DeleteGroundTypeCommandTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.Application.GroundTypes.Command;
+using DiplomaProject.Domain.Exceptions;
+using FluentAssertions;
+using Xunit;
+
+namespace DiplomaProject.Application.UnitTests.GroundTypes.Commands
+{
+    public class DeleteGroundTypeCommandTests : TestBase
+    {
+        [Fact]
+        public async Task ShouldDeleteGroundType()
+        {
+            var command = new DeleteGroundTypeCommand
+            {
+                Id = 1
+            };
+            var handler = new DeleteGroundTypeCommandHandler(ApplicationContext);
+
+            _ = await handler.Handle(command, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task ShouldThrowException_BecauseIdIsIncorrect()
+        {
+            var command = new DeleteGroundTypeCommand
+            {
+                Id = -1
+            };
+            var handler = new DeleteGroundTypeCommandHandler(ApplicationContext);
+
+            Func<Task> func = async () => await handler.Handle(command, CancellationToken.None);
+            await func.Should().ThrowAsync<NotFoundException>();
+        }
+    }
+}

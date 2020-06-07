213960a0adb3fc2ab787c893180077c80b3a6205
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-07 20:52:23 +0300

    Добавить команду создания типа водорослей

---
+++ src/DiplomaProject.Application/SeaweedTypes/Command/CreateSeaweedTypeCommand.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.DataAccess;
+using DiplomaProject.Domain.Entities;
+using MediatR;
+
+namespace DiplomaProject.Application.SeaweedTypes.Command
+{
+    public class CreateSeaweedTypeCommandHandler : IRequestHandler<CreateSeaweedTypeCommand, SeaweedType>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateSeaweedTypeCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<SeaweedType> Handle(CreateSeaweedTypeCommand request, CancellationToken cancellationToken)
+        {
+            var item = new SeaweedType(request.Title);
+
+            await _context.AddAsync(item, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return item;
+        }
+    }
+
+    public class CreateSeaweedTypeCommand : IRequest<SeaweedType>
+    {
+        public string Title { get; set; }
+    }
+}
---
+++ tests/DiplomaProject.Application.UnitTests/SeaweedTypes/Commands/CreateSeaweedCategoryCommandTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.Application.SeaweedTypes.Command;
+using FluentAssertions;
+using Xunit;
+
+namespace DiplomaProject.Application.UnitTests.SeaweedTypes.Commands
+{
+    public class CreateSeaweedTypeCommandTests : TestBase
+    {
+        [Fact]
+        public async Task ShouldCreateSeaweedType()
+        {
+            var command = new CreateSeaweedTypeCommand
+            {
+                Title = "Title"
+            };
+            var handler = new CreateSeaweedTypeCommandHandler(ApplicationContext);
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            result.Id.Should().Be(3);
+        }
+    }
+}

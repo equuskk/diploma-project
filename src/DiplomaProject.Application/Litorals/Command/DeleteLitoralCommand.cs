85e05f8af98b7ab475138e699ad5d6326feb0cf3
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-07 20:51:04 +0300

    Добавить команду удаления литорали

---
+++ src/DiplomaProject.Application/Litorals/Command/DeleteLitoralCommand.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.DataAccess;
+using DiplomaProject.Domain.Entities;
+using DiplomaProject.Domain.Exceptions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiplomaProject.Application.Litorals.Command
+{
+    public class DeleteLitoralCommandHandler : IRequestHandler<DeleteLitoralCommand, Unit>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DeleteLitoralCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteLitoralCommand request, CancellationToken cancellationToken)
+        {
+            var item = await _context.Litorals.SingleOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+            if(item is null)
+            {
+                throw new NotFoundException(request.Id, nameof(Litoral));
+            }
+
+            _context.Remove(item);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+
+    public class DeleteLitoralCommand : IRequest<Unit>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ tests/DiplomaProject.Application.UnitTests/Litorals/Commands/DeleteLitoralCommandTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.Application.Litorals.Command;
+using DiplomaProject.Domain.Exceptions;
+using FluentAssertions;
+using Xunit;
+
+namespace DiplomaProject.Application.UnitTests.Litorals.Commands
+{
+    public class DeleteLitoralCommandTests : TestBase
+    {
+        [Fact]
+        public async Task ShouldDeleteLitoral()
+        {
+            var command = new DeleteLitoralCommand
+            {
+                Id = 1
+            };
+            var handler = new DeleteLitoralCommandHandler(ApplicationContext);
+
+            _ = await handler.Handle(command, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task ShouldThrowException_BecauseIdIsIncorrect()
+        {
+            var command = new DeleteLitoralCommand
+            {
+                Id = -1
+            };
+            var handler = new DeleteLitoralCommandHandler(ApplicationContext);
+
+            Func<Task> func = async () => await handler.Handle(command, CancellationToken.None);
+            await func.Should().ThrowAsync<NotFoundException>();
+        }
+    }
+}

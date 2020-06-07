ce3d95a405bdf01b43561421a85104fd1e145304
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-07 20:52:04 +0300

    Добавить команду создания категории водорослей

---
+++ src/DiplomaProject.Application/SeaweedCategories/Command/CreateSeaweedCategoryCommand.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.DataAccess;
+using DiplomaProject.Domain.Entities;
+using MediatR;
+
+namespace DiplomaProject.Application.SeaweedCategories.Command
+{
+    public class CreateSeaweedCategoryCommandHandler : IRequestHandler<CreateSeaweedCategoryCommand, SeaweedCategory>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateSeaweedCategoryCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<SeaweedCategory> Handle(CreateSeaweedCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var item = new SeaweedCategory(request.Title);
+
+            await _context.AddAsync(item, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return item;
+        }
+    }
+
+    public class CreateSeaweedCategoryCommand : IRequest<SeaweedCategory>
+    {
+        public string Title { get; set; }
+    }
+}
---
+++ tests/DiplomaProject.Application.UnitTests/SeaweedCategories/Commands/CreateSeaweedCategoryCommandTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.Application.SeaweedCategories.Command;
+using FluentAssertions;
+using Xunit;
+
+namespace DiplomaProject.Application.UnitTests.SeaweedCategories.Commands
+{
+    public class CreateSeaweedCategoryCommandTests : TestBase
+    {
+        [Fact]
+        public async Task ShouldCreateSeaweedCategory()
+        {
+            var command = new CreateSeaweedCategoryCommand
+            {
+                Title = "Title"
+            };
+            var handler = new CreateSeaweedCategoryCommandHandler(ApplicationContext);
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            result.Id.Should().Be(3);
+        }
+    }
+}

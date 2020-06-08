4878651d8d48c80a15c28561565dc022cc54ce40
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-08 17:24:37 +0300

    Добавить команду создания водорослей

---
+++ src/DiplomaProject.Application/Seaweeds/Commands/CreateSeaweedCommand.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.DataAccess;
+using DiplomaProject.Domain.Entities;
+using MediatR;
+
+namespace DiplomaProject.Application.Seaweeds.Commands
+{
+    public class CreateSeaweedCommandHandler : IRequestHandler<CreateSeaweedCommand, Seaweed>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateSeaweedCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Seaweed> Handle(CreateSeaweedCommand request, CancellationToken cancellationToken)
+        {
+            var seaweed = new Seaweed
+            {
+                Title = request.Title,
+                SeaweedCategoryId = request.SeaweedCategoryId,
+                SeaweedTypeId = request.SeaweedTypeId
+            };
+
+            await _context.Seaweeds.AddAsync(seaweed, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return seaweed;
+        }
+    }
+
+    public class CreateSeaweedCommand : IRequest<Seaweed>
+    {
+        public string Title { get; set; }
+
+        public int SeaweedTypeId { get; set; }
+        public int SeaweedCategoryId { get; set; }
+    }
+}
---
+++ tests/DiplomaProject.Application.UnitTests/Seaweeds/Commands/CreateSeaweedCommandTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.Application.Seaweeds.Commands;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace DiplomaProject.Application.UnitTests.Seaweeds.Commands
+{
+    public class CreateSeaweedCommandTests : TestBase
+    {
+        [Fact]
+        public async Task ShouldCreateSeaweed()
+        {
+            var command = new CreateSeaweedCommand
+            {
+                Title = "1",
+                SeaweedCategoryId = 1,
+                SeaweedTypeId = 1
+            };
+            var handler = new CreateSeaweedCommandHandler(ApplicationContext);
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            result.Id.Should().Be(3);
+        }
+
+        [Fact]
+        public async Task ShouldThrowException_BecauseCategoryIsIncorrect()
+        {
+            var command = new CreateSeaweedCommand
+            {
+                Title = "1",
+                SeaweedCategoryId = -1,
+                SeaweedTypeId = 1
+            };
+            var handler = new CreateSeaweedCommandHandler(ApplicationContext);
+
+            Func<Task> func = async () => await handler.Handle(command, CancellationToken.None);
+
+            await func.Should().ThrowAsync<DbUpdateException>();
+        }
+
+        [Fact]
+        public async Task ShouldThrowException_BecauseTypeIsIncorrect()
+        {
+            var command = new CreateSeaweedCommand
+            {
+                Title = "1",
+                SeaweedCategoryId = 1,
+                SeaweedTypeId = -1
+            };
+            var handler = new CreateSeaweedCommandHandler(ApplicationContext);
+
+            Func<Task> func = async () => await handler.Handle(command, CancellationToken.None);
+
+            await func.Should().ThrowAsync<DbUpdateException>();
+        }
+    }
+}

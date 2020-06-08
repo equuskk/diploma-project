bf2fce140098c290eb6c1d5d05e758f0d658791c
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-08 17:47:30 +0300

    Добавить команду удаления водоросли

---
+++ src/DiplomaProject.Application/Seaweeds/Commands/DeleteSeaweedCommand.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.DataAccess;
+using DiplomaProject.Domain.Entities;
+using DiplomaProject.Domain.Exceptions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiplomaProject.Application.Seaweeds.Commands
+{
+    public class DeleteSeaweedCommandHandler : IRequestHandler<DeleteSeaweedCommand, Unit>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DeleteSeaweedCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteSeaweedCommand request, CancellationToken cancellationToken)
+        {
+            var item = await _context.Seaweeds.SingleOrDefaultAsync(x => x.Id == request.SeaweedId,
+                                                                    cancellationToken);
+            if(item is null)
+            {
+                throw new NotFoundException(request.SeaweedId, nameof(Seaweed));
+            }
+
+            _context.Remove(item);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+
+    public class DeleteSeaweedCommand : IRequest<Unit>
+    {
+        public int SeaweedId { get; set; }
+    }
+}
---
+++ tests/DiplomaProject.Application.UnitTests/Seaweeds/Commands/DeleteSeaweedCommandTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.Application.Seaweeds.Commands;
+using DiplomaProject.Domain.Exceptions;
+using FluentAssertions;
+using Xunit;
+
+namespace DiplomaProject.Application.UnitTests.Seaweeds.Commands
+{
+    public class DeleteSeaweedCommandTests : TestBase
+    {
+        [Fact]
+        public async Task ShouldDeleteLitoral()
+        {
+            var command = new DeleteSeaweedCommand
+            {
+                SeaweedId = 1
+            };
+            var handler = new DeleteSeaweedCommandHandler(ApplicationContext);
+
+            _ = await handler.Handle(command, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task ShouldThrowException_BecauseIdIsIncorrect()
+        {
+            var command = new DeleteSeaweedCommand
+            {
+                SeaweedId = -1
+            };
+            var handler = new DeleteSeaweedCommandHandler(ApplicationContext);
+
+            Func<Task> func = async () => await handler.Handle(command, CancellationToken.None);
+            await func.Should().ThrowAsync<NotFoundException>();
+        }
+    }
+}

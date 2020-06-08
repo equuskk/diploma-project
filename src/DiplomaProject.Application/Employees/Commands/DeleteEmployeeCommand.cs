a1637ab5ddde58fb498f471c5401d5ad59fb2354
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-08 14:46:32 +0300

    Добавить команду и страничку админа с удалением сотрудников

---
+++ src/DiplomaProject.Application/Employees/Commands/DeleteEmployeeCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.Domain.Entities;
+using DiplomaProject.Domain.Exceptions;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace DiplomaProject.Application.Employees.Commands
+{
+    public class DeleteEmployeeCommandHandler : IRequestHandler<DeleteEmployeeCommand, Unit>
+    {
+        private readonly UserManager<Employee> _userManager;
+
+        public DeleteEmployeeCommandHandler(UserManager<Employee> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<Unit> Handle(DeleteEmployeeCommand request, CancellationToken cancellationToken)
+        {
+            var item = await _userManager.FindByIdAsync(request.EmployeeId);
+            if(item is null)
+            {
+                throw new NotFoundException(request.EmployeeId, nameof(Employee));
+            }
+
+            var deleteResult = await _userManager.DeleteAsync(item);
+            if(deleteResult.Succeeded)
+            {
+                return Unit.Value;
+            }
+
+            throw new ArgumentException(); // TODO:
+        }
+    }
+
+    public class DeleteEmployeeCommand : IRequest<Unit>
+    {
+        public string EmployeeId { get; set; }
+    }
+}
--- src/DiplomaProject.Domain/Exceptions/NotFoundException.cs
+++ src/DiplomaProject.Domain/Exceptions/NotFoundException.cs
@@ -4,7 +4,7 @@ namespace DiplomaProject.Domain.Exceptions
 {
     public class NotFoundException : Exception
     {
-        public NotFoundException(int id, string entityType) : base($"Сущность {entityType} с идентификатором {id} не найдена")
+        public NotFoundException(object id, string entityType) : base($"Сущность {entityType} с идентификатором {id} не найдена")
         {
         }
     }

4a81bc92b25ad5fce3c36c43ab2b8d94c2db055c
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-08 18:35:50 +0300

    Добавить команду изменения сектора

---
+++ src/DiplomaProject.Application/Sectors/Commands/UpdateSectorCommand.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.DataAccess;
+using DiplomaProject.Domain.Entities;
+using DiplomaProject.Domain.Exceptions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiplomaProject.Application.Sectors.Commands
+{
+    public class UpdateSectorCommandHandler : IRequestHandler<UpdateSectorCommand, Sector>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UpdateSectorCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Sector> Handle(UpdateSectorCommand request, CancellationToken cancellationToken)
+        {
+            var sector = await _context.Sectors.SingleOrDefaultAsync(x => x.Id == request.SectorId, cancellationToken);
+            if(sector is null)
+            {
+                throw new NotFoundException(request.SectorId, nameof(Sector));
+            }
+
+            sector.Title = request.Title;
+            sector.Description = request.Description;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return sector;
+        }
+    }
+
+    public class UpdateSectorCommand : IRequest<Sector>
+    {
+        public int SectorId { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ tests/DiplomaProject.Application.UnitTests/Sectors/Commands/UpdateSectorCommandTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DiplomaProject.Application.Sectors.Commands;
+using DiplomaProject.Domain.Exceptions;
+using FluentAssertions;
+using Xunit;
+
+namespace DiplomaProject.Application.UnitTests.Sectors.Commands
+{
+    public class UpdateSectorCommandTests : TestBase
+    {
+        [Fact]
+        public async Task ShouldThrowException()
+        {
+            var command = new UpdateSectorCommand
+            {
+                SectorId = -1,
+                Title = "1",
+                Description = "1"
+            };
+            var handler = new UpdateSectorCommandHandler(ApplicationContext);
+
+            Func<Task> func = async () => await handler.Handle(command, CancellationToken.None);
+            await func.Should().ThrowAsync<NotFoundException>();
+        }
+
+        [Fact]
+        public async Task ShouldUpdateSector()
+        {
+            const string title = "Title234";
+            const string description = "Description234";
+            var command = new UpdateSectorCommand
+            {
+                SectorId = 1,
+                Title = title,
+                Description = description
+            };
+            var handler = new UpdateSectorCommandHandler(ApplicationContext);
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            result.Id.Should().Be(1);
+            result.Title.Should().Be(title);
+            result.Description.Should().Be(description);
+        }
+    }
+}
